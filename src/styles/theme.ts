ef169eb93e53157c4893fb31d345d374f7bdaf86
Alexandre Kimura <alexandremkimura@hotmail.com>
2021-10-14 14:40:45 -0300

    Config fonts

---
+++ src/styles/theme.ts
@@ -0,0 +1,14 @@
+import { extendTheme } from '@chakra-ui/react'
+
+export const theme = extendTheme({
+  colors: {
+
+  },
+  styles: {
+    global: {
+      body: {
+        bg: '#fff'
+      }
+    }
+  }
+})
